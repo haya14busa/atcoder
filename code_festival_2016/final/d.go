4faff58b0c6af6b4a9ff12c5c81fd22832648936
haya14busa <hayabusa1419@gmail.com>
2016-11-26 21:09:50 +0900

    hmmm...

--- code_festival_2016/final/d.go
+++ code_festival_2016/final/d.go
@@ -18,65 +18,127 @@ func main() {
 	}
 }
 
+type RemCnt struct {
+	Same map[int]int
+	Diff int
+	Num  int
+}
+
 func solve(r io.Reader, w io.Writer) {
 	var n, m int
 	fmt.Fscanln(r, &n, &m)
 	xcnt := make(map[int]int, n)
-	max := 0
 	for i := 0; i < n; i++ {
 		var x int
 		fmt.Fscan(r, &x)
 		xcnt[x]++
-		if x > max {
-			max = x
-		}
 	}
 
-	max = max * 2
+	rm := make(map[int]*RemCnt)
 
-	pairNum := 0
-
-	for i := 1; m*i <= max; i++ {
-		mi := m * i
-		for x, cnt := range xcnt {
-			if cnt < 1 {
-				delete(xcnt, x)
-				continue
-			}
-			want := mi - x
-			if want < 1 || (x == want) || cnt > 1 {
-				continue
-			}
-			if c, ok := xcnt[want]; ok && c > 0 {
-				xcnt[x]--
-				xcnt[want]--
-				pairNum++
-				fmt.Println("pair", x, want)
-			}
-		}
-	}
+	ones := make([]int, 0)
 
 	for x, cnt := range xcnt {
+		r := x % m
+		if _, ok := rm[r]; !ok {
+			rm[r] = &RemCnt{}
+		}
+		rm[r].Num += cnt
 		if cnt > 1 {
-			pairNum += cnt / 2
-			fmt.Println("pair", x, x, " x ", cnt/2)
+			if rm[r].Same == nil {
+				rm[r].Same = make(map[int]int)
+			}
+			rm[r].Same[x] += cnt
+		} else {
+			rm[r].Diff += cnt
+			ones = append(ones, x)
+		}
+	}
 
-			if cnt%2 == 1 {
-				for i := 1; m*i <= max; i++ {
-					mi := m * i
-					want := mi - x
-					if want < 1 {
-						continue
-					}
-					if c, ok := xcnt[want]; ok && c > 0 {
-						xcnt[x]--
-						xcnt[want]--
-						pairNum++
-						fmt.Println("pair", x, want)
+	pairNum := 0
+	for _, x := range ones {
+		xm := x % m
+		want := m - xm
+		if m == want {
+			want = 0
+		}
+		if xm == want {
+			if rm[want].Diff > 1 {
+				rm[want].Num -= 2
+				rm[want].Diff -= 2
+				pairNum++
+				// fmt.Println("pair", x, "?")
+			} else {
+				y := -1
+				for xthat, cnt := range rm[want].Same {
+					y = xthat
+					if cnt%2 == 1 {
 						break
 					}
 				}
+				if y != -1 {
+					rm[want].Num -= 2
+					rm[want].Diff--
+					rm[want].Same[y]--
+					pairNum++
+					// fmt.Println("pair", x, y)
+				}
 			}
+			continue
+		}
+		if c, ok := rm[want]; ok {
+			if c.Diff > 0 {
+				c.Num--
+				c.Diff--
+				rm[xm].Num--
+				rm[xm].Diff--
+				pairNum++
+				// fmt.Println("pair", x, "?")
+			} else {
+				y := -1
+				for xthat, cnt := range c.Same {
+					y = xthat
+					if cnt%2 == 1 {
+						break
+					}
+				}
+				if y != -1 {
+					c.Num--
+					c.Same[y]--
+					rm[xm].Num--
+					rm[xm].Diff--
+					pairNum++
+					// fmt.Println("pair", x, y)
+				}
+			}
+		}
+	}
+
+	for x, c := range rm {
+		if c.Num == 0 {
+			delete(rm, x)
+			continue
+		}
+		xm := x % m
+		want := m - xm
+		if m == want {
+			want = 0
+		}
+		if cthat, ok := rm[want]; ok && cthat.Num > 0 {
+			num := c.Num
+			if num > cthat.Num {
+				num = cthat.Num
+			}
+			plus := num / 2
+			c.Num -= plus
+			cthat.Num -= plus
+			pairNum += plus
+			// fmt.Printf("%d cthat: %#v\n", want, cthat)
+		}
+		if plus := (c.Num - c.Diff) / 2; plus > 0 {
+			pairNum += plus
+			c.Num -= plus * 2
+			// fmt.Println("pair", x, x)
 		}
 	}
 
@@ -97,10 +159,10 @@ func test() {
 		},
 		{
 			in: `
-15 10
-1 5 6 10 11 11 11 20 21 25 25 26 99 99 99
+17 10
+1 5 6 10 11 11 11 20 21 25 25 26 99 99 99 19 19
 `,
-			want: `6`,
+			want: `7`,
 		},
 	}
 	for i, tt := range tests {
